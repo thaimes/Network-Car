cd01f192ac39f919d47105a75d42cfe51c8fcccc
thaimes <thomasjhaimes@gmail.com>
2025-06-10 18:11:32 -0500

    Create UDP_Server.py
    UDP Server for receiving information from UDP Client.

---
+++ UDP_Server.py
@@ -0,0 +1,23 @@
+# UDP Server code to listen for UDP client on PC
+
+import socket
+
+UDP_IP = "0.0.0.0" # IPv4 IP for PC
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    #print("recieved message: %s" % data )
+    
+    if data == b'LEFT':
+        print("TURNING LEFT")
+    if data == b'RIGHT':
+        print("TURNING RIGHT")
+    if data == b'BACK':
+        print("BACKING UP")
+    if data == b'FORWARD':
+        print("GOING FORWARD")
+
