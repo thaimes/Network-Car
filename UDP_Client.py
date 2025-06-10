914c3d646d29979253bee1e0845c57da0481b359
thaimes <thomasjhaimes@gmail.com>
2025-06-10 18:01:19 -0500

    Create UDP_Client.py
    UDP Client to send messages to UDP Server.

---
+++ UDP_Client.py
@@ -0,0 +1,43 @@
+# UDP Client code to talk to the UDP server on the Rpi 5
+
+import socket
+import pygame
+import sys
+
+
+UDP_IP = "172.20.10.4" # IPv4 for Rpi 5
+UDP_PORT = 5005
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+#print("message: %s" % MESSAGE)
+
+# Initialize Pygame
+pygame.init()
+
+display = pygame.display.set_mode((300, 300))
+
+
+while True:
+    MESSAGE = None # Blank message to start with
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                MESSAGE = b"LEFT"
+            if event.key == pygame.K_RIGHT:
+                MESSAGE = b"RIGHT"
+            if event.key == pygame.K_DOWN:
+                MESSAGE = b"BACK"
+            if event.key == pygame.K_UP:
+                MESSAGE = b"FORWARD"
+            
+    if MESSAGE != None:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+
+
