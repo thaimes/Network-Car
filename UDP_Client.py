a4ef3a0fbc504ef2b8e1478bd783efd8c9aeffb3
thaimes <thomasjhaimes@gmail.com>
2025-07-08 20:41:47 -0500

    Update UDP_Client.py

--- UDP_Client.py
+++ UDP_Client.py
@@ -1,11 +1,9 @@
 # Controller test code to replace keyboard code
-
 import socket
 import pygame
 import sys
-import time
 
-UDP_IP = "172.20.10.4" # IPv4 for Rpi 5
+UDP_IP = "172.20.10.7" # IPv4 for Rpi 5
 UDP_PORT = 5005
 
 print("UDP target IP: %s" % UDP_IP)
@@ -15,63 +13,76 @@ pygame.init()
 pygame.joystick.init()
 
 display = pygame.display.set_mode((300, 300))
+pygame.display.set_caption("Controller Input Test")
 
 if pygame.joystick.get_count() == 0:
-    print("No joystick found!")
+    print("No joystick found. Please connect a joystick.")
+    pygame.quit()
     sys.exit()
 
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 print("Joystick name:", joystick.get_name())
 
-# Joystick axis mapping
-L_X = 0
-L_TRIG = 4
-R_TRIG = 5
-
-# Track latest axis values
-axis_values = {
-    L_X: 0.0,
-    L_TRIG: 0.0,
-    R_TRIG: 0.0
-}
+lt_pressed = False  # Track LT state
+rt_pressed = False  # Track RT state
+lsl_pressed = False  # Track LS state left
+lsr_pressed = False  # Track LS state right
+last_action = None  # Track last printed action
 
 while True:
-    MESSAGE = None
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.JOYAXISMOTION:
-            axis_values[event.axis] = event.value  # Value tracking
+    
+    lt_value = joystick.get_axis(4)
+    rt_value = joystick.get_axis(5)
+    ls_value = joystick.get_axis(0)
 
-    MESSAGE = b""
+    # Determine combined states
+    action = None
+    MESSAGE = None
 
-    lx = axis_values[L_X]
-    lt = axis_values[L_TRIG]
-    rt = axis_values[R_TRIG]
-
-    if rt > 0.5 and lx < -0.5:
-        MESSAGE = b"FORWARD AND LEFT"
-    elif rt > 0.5 and lx > 0.5:
-        MESSAGE = b"FORWARD AND RIGHT"
-    elif lt > 0.5 and lx < -0.5:
-        MESSAGE = b"BACKING AND RIGHT"
-    elif lt > 0.5 and lx > 0.5:
-        MESSAGE = b"BACKING AND LEFT"
-    elif rt > 0.5:
-        MESSAGE = b"FORWARD"
-    elif lt > 0.5:
+    if rt_value > 0.5 and ls_value < -0.5:
+        action = "FORWARD AND LEFT"
+        MESSAGE = b"FLEFT"
+    elif rt_value > 0.5 and ls_value > 0.5:
+        action = "FORWARD AND RIGHT"
+        MESSAGE = b"FRIGHT"
+    elif lt_value > 0.5 and ls_value < -0.5:
+        action = "BACKWARD AND LEFT"
+        MESSAGE = b"BLEFT"
+    elif lt_value > 0.5 and ls_value > 0.5:
+        action = "BACKWARD AND RIGHT"
+        MESSAGE = b"BRIGHT"
+    elif rt_value > 0.5:
+        action = "FORWARD"
+        MESSAGE = b"FORWARD"        
+    elif lt_value > 0.5:
+        action = "BACK"
         MESSAGE = b"BACK"
-    elif lx < -0.5:
+    elif ls_value < -0.5:
+        action = "LEFT"
         MESSAGE = b"LEFT"
-    elif lx > 0.5:
+    elif ls_value > 0.5:
+        action = "RIGHT"
         MESSAGE = b"RIGHT"
+    else:
+        action = "HALT"
+        MESSAGE = b""
 
-   # time.sleep(0.1)
+    if last_action != action:
+        print(action)
+        last_action = action
+
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
-# Small issues
-# - Currently has delay between messages
+
+
+# Controller mapping (PS5 controller being used, update to XBOX controller at lab)
+# LT = Axis 4
+# RT = Axis 5
+# LS = Axis 0
